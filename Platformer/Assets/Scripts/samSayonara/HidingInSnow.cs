fdc8123d240e959d57d1289607872948efa44e2c
SamSayo <dima.romanov254@gmail.com>
2024-06-30 10:37:53 +0400

    Added i dunno what

---
+++ Platformer/Assets/Scripts/samSayonara/HidingInSnow.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using Platformer.Gameplay;
+using static Platformer.Core.Simulation;
+
+namespace Platformer.Mechanics
+{
+    [RequireComponent(typeof(Collider2D))]
+    public class SnowDriftInstance : MonoBehaviour
+    {
+        
+    }
+}
